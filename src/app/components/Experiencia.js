36d770e40f0c61a77da98d91d1996faf5cbc052b
Facundo Ballesteros <facundoballesteros01@hotmail.com>
2020-12-16 07:02:20 -0300

    ClassName error

--- src/app/components/Experiencia.js
+++ src/app/components/Experiencia.js
@@ -7,7 +7,7 @@ export default class Experiencia extends React.Component {
             <div className="section scrollspy" id="experience">
                 <div className="container">
                     <h2>Experiencia</h2>
-                    <div class="timeline">
+                    <div className="timeline">
                         <ExperienciaItem anio="2017" texto="Aprendo de manera autodiacta HTML y CSS y empiezo a realizar trabajos freelance."/>
                         <ExperienciaItem anio="2018" texto="Termino la Tecnicatura en Diseño y Animación Digital con promedio de 9.11."/>
                         <ExperienciaItem anio="2019" texto="Realiza cursos en Acamica, Plan111Mil <br />Comienzo a trabajar con PHP, Codeigniter y MySql como freelancer, aplicando estas tecnologia en proyectos para clientes del sector privado"/>
